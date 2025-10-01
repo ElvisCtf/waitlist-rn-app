456380536eed7015e2ca122e01e3b4026db47303
elvisctf <elvischengtf@gmail.com>
2025-10-01 19:59:51 +0800

    feat: Main screen UI

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,7 +1,11 @@
 import { Text, View } from "react-native";
+import Main from "../screens/Main";
+import { SafeAreaProvider } from "react-native-safe-area-context";
 
 export default function Index() {
   return (
-    <View></View>
+    <SafeAreaProvider>
+      <Main />
+    </SafeAreaProvider>
   );
 }
